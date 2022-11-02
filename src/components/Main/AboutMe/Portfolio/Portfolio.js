34f5c5f116cce6c0d0f3582545799d9bda03f4fc
Kira Gorinchoi <voronova.kira@gmail.com>
2022-11-02 14:22:09 +0300

    review ver.2

--- src/components/Main/AboutMe/Portfolio/Portfolio.js
+++ src/components/Main/AboutMe/Portfolio/Portfolio.js
@@ -5,24 +5,26 @@ function Portfolio() {
     return (
         <div className='portfolio'>
             <h2 className='portfolio__title'>Портфолио</h2>
-            <div className='portfolio__link-container'>
-                <a className='portfolio__link' href='https://github.com/paradoxicallly/how-to-learn' target="blank">Статичный сайт</a>
-                <a className='portfolio__link-image' href='https://github.com/paradoxicallly/how-to-learn' target="blank">
-                    <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
-                </a>
-            </div>
-            <div className='portfolio__link-container'>
-                <a className='portfolio__link' href='https://github.com/paradoxicallly/russian-travel' target="blank">Адаптивный сайт</a>
-                <a className='portfolio__link-image' href='https://github.com/paradoxicallly/russian-travel' target="blank">
-                    <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
-                </a>
-            </div>
-            <div className='portfolio__link-container'>
-                <a className='portfolio__link' href='https://github.com/paradoxicallly/react-mesto-api-full' target="blank">Одностраничное приложение</a>
-                <a className='portfolio__link-image' href='https://github.com/paradoxicallly/react-mesto-api-full' target="blank">
-                    <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
-                </a>
-            </div>
+            <ul className='portfolio__container'>
+                <li className='portfolio__link-container'>
+                    <a className='portfolio__link' href='https://github.com/paradoxicallly/how-to-learn' target="blank">Статичный сайт</a>
+                    <a className='portfolio__link-image' href='https://github.com/paradoxicallly/how-to-learn' target="blank">
+                        <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
+                    </a>
+                </li>
+                <li className='portfolio__link-container'>
+                    <a className='portfolio__link' href='https://github.com/paradoxicallly/russian-travel' target="blank">Адаптивный сайт</a>
+                    <a className='portfolio__link-image' href='https://github.com/paradoxicallly/russian-travel' target="blank">
+                        <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
+                    </a>
+                </li>
+                <li className='portfolio__link-container'>
+                    <a className='portfolio__link' href='https://github.com/paradoxicallly/react-mesto-api-full' target="blank">Одностраничное приложение</a>
+                    <a className='portfolio__link-image' href='https://github.com/paradoxicallly/react-mesto-api-full' target="blank">
+                        <img className='portfolio__image' src={portfolioLink} alt='ссылка' />
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
